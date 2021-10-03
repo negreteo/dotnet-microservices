3608b83478917d212fa4fc8f6817a2103ec83903
Oscar Negrete <oscar.negrete09@gmail.com>
2021-10-03 14:57:23 -0400

    Added packages and commands platforms controller

---
+++ CommandsService/Controllers/PlatformsController.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CommandsService.Controllers
+{
+  [Route("api/commands/[controller]")]
+  [ApiController]
+  public class PlatformsController : ControllerBase
+  {
+    public PlatformsController()
+    {
+    }
+
+    [HttpPost]
+    public ActionResult TestInboundConnection()
+    {
+      Console.WriteLine($"--> Inbound POST # Command Service");
+      return Ok("Inbound test from Platforms controller");
+    }
+  }
+}
